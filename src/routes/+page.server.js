a13946b312bb7341f4aa6e73aaceb6a778a195b2
Luis Rdgz Marenco <lgrodriguez1987@gmail.com>
2023-01-21 03:15:54 -0600

    Adding cards to the main screen

---
+++ src/routes/+page.server.js
@@ -0,0 +1,19 @@
+import { error } from '@sveltejs/kit';
+import { serializeNonPOJOs } from '$lib/utils';
+
+export const load = ({ locals }) => {
+	const getProjects = async () => {
+		try {
+			const projects = serializeNonPOJOs(
+				await locals.pb.collection('projects').getFullList(undefined)
+			);
+			return projects;
+		} catch (err) {
+			console.log('Error: ', err);
+			throw error(err.status, err.messages);
+		}
+	};
+	return {
+		projects: getProjects()
+	};
+};
